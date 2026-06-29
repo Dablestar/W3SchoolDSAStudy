b7e594e6b6048af007132e9f07aba246a7c0a8b0
Dablestar <jbynchoi@gmail.com>
2026-06-29 19:56:21 +1000

    Part 2 - Bubble Sort

---
+++ Arrays/bubblesort.py
@@ -0,0 +1,31 @@
+# Bubble Sort
+# 1) Go through the array, one value at one time
+# 2) For each value, compare with next item
+# 3) if the current item is bigger/smaller, switch the index
+# 4) go through until the array is sorted
+
+# Time Complexity - O(n^2)
+
+my_array = [ 7,12,9,11,3 ]
+n = len(my_array)
+for i in range(n-1):
+    for j in range (n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+
+print(f'result: {my_array}')
+
+# Improvement - Swap Indicator
+# if swap didn't happen, skip the rest of process => reduce redundant process
+
+my_array2 = [7, 3, 9, 12, 11]
+
+o = len(my_array2)
+for i in range(n-1):
+    swapped = False
+    for j in range(n-i-1):
+        if my_array2[j] > my_array2[j+1]:
+            my_array2[j], my_array2[j+1] = my_array2[j+1], my_array2[j]
+            swapped = True
+    if not swapped:
+        break
